88ca2855584c4ec158e5ec2fbf8e62be5c3c2bd3
Sugam Adhikari <I37096@verisk.com>
2023-04-09 16:43:24 +0545

    Refactor: abstract out footer and social buttons into components

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { email, phone } from '../info'
+</script>
+
+<footer class="flex flex-col items-center">
+	<div
+		class="flex w-3/4 max-w-[1080px] mt-20 border-b border-gray-400 border-b-1"
+	/>
+	<div
+		class="flex flex-col items-center justify-center mt-10 mb-10 space-y-2 font-thin text-center md:space-y-0 md:flex-row md:space-x-6"
+	>
+		<p class="">{email}</p>
+		<p class="hidden md:block">•</p>
+		<p>{phone}</p>
+	</div>
+</footer>
---
+++ src/lib/info.ts
@@ -0,0 +1,10 @@
+// Personal info
+export const name = 'Sugam Adhikari';
+export const bio = 'Just a regular software engineer obsessed with Real Madrid';
+export const email = 'sugam.adhiakri123@gmail.com';
+export const phone = '+977 9843-(sike)';
+
+
+// Social Links
+export const github = 'https://github.com/sugamadhiakri';
+export const linkedin = 'https://linkedin.com/in/sugamadhiakri';
